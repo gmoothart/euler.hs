092d8d7c48bfbe4ab8d4837b431fc7413a7f0edf
gmoothart <gmoothart@gmail.com>
2009-03-29 21:55:08 -0700

    fixed syntax errors and bugs. Problem 1 solved!

--- 1.hs
+++ 1.hs
@@ -1,31 +1,34 @@
 --
 -- Euler project #1
--- The syntax is incorrect... but I know what I want to do!
+-- 
+
+import List
 
 --helper methods
 div_3 x = (mod x 3) == 0
 div_5 x = (mod x 5) == 0
 
-sum_first_1k x = sum (takeWhile (<= 1000) x)
+sum_first_1k x = sum (takeWhile (< 1000) x)
 
 --  sum multiples of 3, and add multiples of 5 that are not also
 --  multiples of 3
-threes1 = [x*3 | x <- [1..], div_3 x]
-fives1  = [x*5 | x <- [1..], (div_5 x) == True && (div_3 x) == False]
+threes1 = [x*3 | x <- [1..]]
+fives1  = [x*5 | x <- [1..], (div_3 (x*5)) == False]
 
 
 --using addition instead of multiplication
 threes2 = 3 : [ x+3 | x <- threes2]
-fives2 = 5 : [ if div_3 x then x+10 else x+5 | x <- fives2 ]
+fives2 = 5 : [ if div_3 (x+5) then x+10 else x+5 | x <- fives2 ]
 
 -- same as above, but via map and pattern-matching
-next x | div_3 x   = x+10 -- if x is a mutiple of 3, go to the next one
-       | otherwise = x+5
+next x | div_3 (x+5)   = x+10 -- if x is a mutiple of 3, go to the next one
+       | otherwise     = x+5
 fives2b  = 5 : [ x | x <- map next fives2b]
 
 -- using the iterate function instead of list comprehensions
-threes3 = (iterate (+3) 0)
-fives3  = (iterate (+5) 0)
+threes3 = (iterate (+3) 3)
+fives3  = (iterate (+5) 5)
+both3   = (iterate (+15) 15)
 
 --Just for fun:
 fibs = 0 : 1 : [a + b| (a, b) <- zip fibs (tail fibs)]
@@ -33,7 +36,7 @@ fibs = 0 : 1 : [a + b| (a, b) <- zip fibs (tail fibs)]
 main = do
          --brute-force
          --  check every number from 3 to 1000
-         print (sum (filter (\x -> (div_3 x) || (div_5 x)) [3..1000]))
+         print (sum (filter (\x -> (div_3 x) || (div_5 x)) [3..999]))
          --smarter: generate list by multiplication
          --  i.e., 1,2,3 -> 3,6,9
          print ((sum_first_1k threes1) + (sum_first_1k fives1))
@@ -41,6 +44,9 @@ main = do
          print ((sum_first_1k threes2) + (sum_first_1k fives2))
          print ((sum_first_1k threes2) + (sum_first_1k fives2b))
          --using the iterate function
-         print ((sum_first_1k threes3) + (sum_first_1k fives3))
+         print ((sum_first_1k threes3) + sum ((takeWhile (<1000) fives3) \\ (takeWhile (<1000) both3)))
          --just for fun:
          print (take 10 fibs)
+
+
+
