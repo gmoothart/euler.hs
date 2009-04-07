796203cb838070f16c6f7701015c252c191fcd2c
gmoothart <gmoothart@gmail.com>
2009-04-06 21:09:13 -0700

    added a tail-recursive version using accumulators. Still no go

--- 2.hs
+++ 2.hs
@@ -4,12 +4,29 @@
  - Is there a clever way to do this?
  -
  - -}
-fib   :: Int -> Int
-fib 0 =  0
-fib 1 =  1
-fib n =  (fib n-1) + fib(n-2)
 
-flist = map (fib) [1..4000000]
-evens = filter (\n -> (mod n 2) == 0) flist 
+-- To avoid a stack overflow, use accumulators.
+-- b1 and b2 are the previous fibs, sum is the running total,
+-- max is the number of fibs to test, n is the current fib
+fib_evens_sum_r                 :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer  
+fib_evens_sum_r n b1 b2 sum max 
+  | max <= 2      = error "must choose n > 2!"
+  | n == max      = sum
+  | otherwise     = fib_evens_sum_r (n+1) b2 (b2+b1) (if (even (b1+b2)) then sum+b1+b2 else sum ) max
+
+fib_evens_sum max = fib_evens_sum_r 2 1 2 2 max
+
+fibs :: [Integer]
+fibs =  1 : 2 : [ a+b | (a,b) <- zip fibs (tail fibs)]
+
+evens  :: Integer -> Integer -> Integer
+evens x sum | (even x)   = x + sum
+            | otherwise  = sum
+
+
 main = do
-  print (sum (evens))
+  print (fib_evens_sum 10)
+  print ( sum (filter (\x -> x `mod` 2 == 0) (take 10 fibs) ) )
+  -- I thought this one would work...
+  --print (foldr evens 0 (take 10 fibs))
+  --return () >>= \_ -> print (foldr evens 0 (take 4000000 fibs))
