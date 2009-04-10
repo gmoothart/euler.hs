e693561225bde81840848ce1a42b2f38d0a116da
Gabe Moothart <gmoothart@gmail.com>
2009-04-10 07:40:27 -0700

    solved hugs 2.hs I misread the problem...

--- 2.hs
+++ 2.hs
@@ -5,13 +5,13 @@
  -
  - -}
 
--- To avoid a stack overflow, use accumulators.
+-- Using accumulators and explicit recursion
 -- b1 and b2 are the previous fibs, sum is the running total,
 -- max is the number of fibs to test, n is the current fib
 fib_evens_sum_r                 :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer  
 fib_evens_sum_r n b1 b2 sum max 
   | max <= 2      = error "must choose n > 2!"
-  | n == max      = sum
+  | b1+b2 > 144   = sum
   | otherwise     = fib_evens_sum_r (n+1) b2 (b2+b1) (if (even (b1+b2)) then sum+b1+b2 else sum ) max
 
 fib_evens_sum max = fib_evens_sum_r 2 1 2 2 max
@@ -25,8 +25,7 @@ evens x sum | (even x)   = x + sum
 
 
 main = do
+  print (take 20 fibs)
+  print (foldr evens 0 (takeWhile (<= 144) fibs))
   print (fib_evens_sum 10)
-  print ( sum (filter (\x -> x `mod` 2 == 0) (take 10 fibs) ) )
-  -- I thought this one would work...
-  --print (foldr evens 0 (take 10 fibs))
-  --return () >>= \_ -> print (foldr evens 0 (take 4000000 fibs))
+  print ( sum (filter (\x -> x `mod` 2 == 0) (takeWhile (<= 144) fibs) ) )
