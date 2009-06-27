789224a935ff3d0c8bd055c9bafc34d6b689ffd9
gmoothart <gmoothart@gmail.com>
2009-06-26 21:52:55 -0700

    solved project euler problem 3

---
+++ 3.hs
@@ -0,0 +1,28 @@
+
+-- Project Euler #3
+--What is the largest prime factor of the number 600851475143 ?
+
+
+n = 600851475143
+
+--primes sieve, from http://www.haskell.org/haskellwiki/Prime_numbers
+all_primes :: [Integer]
+all_primes = 2:3:primes'
+  where
+      1:p:candidates = [6*k+r | k <- [0..], r <- [1,5]]
+      primes'        = p : filter isPrime candidates
+      isPrime n      = all (not . divides n) $ takeWhile (\p -> p*p <= n) primes'
+      divides n p    = n `mod` p == 0
+
+
+-- could we re-write this using fold?
+lpf :: Integer -> [Integer] -> Integer
+lpf n primes 
+  | p == n             = p
+  | n `mod` p == 0     = lpf (n `div` p) (tail primes)
+  | otherwise          = lpf n     (tail primes)
+  where p = (head primes)
+
+main = do
+  print (lpf n all_primes)
+
