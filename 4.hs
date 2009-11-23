99930c3ce3ccb80593c6e04c0fbb87f3d4b3463e
Gabe Moothart <gmoothart@gmail.com>
2009-11-23 08:34:55 -0800

    fixed compile errors. Works!

--- 4.hs
+++ 4.hs
@@ -2,23 +2,26 @@
 -- Project Euler #4:
 -- Find the largest palindromic number divisible by two 3-digit numbers
 
-isPalindrome :: Integer -> Boolean
-isPalindrome x = isPalindromeString ()
-  where isPalindromeString :: [Char] -> Boolean
-        isPalindromeString []   = True
-        isPalindromeString a:[] = True
-        isPalindromeString s    = (first s) == (last s) && (isPalindrome inner)
-          where inner = rest (init s)
+import List
 
-isDivisible :: Integer -> Boolean
-isDivisible x = any [999...(sqrt x)] \y-> x `mod` y == 0
+isPalindrome :: Integer -> Bool
+isPalindrome x = isPalindromeString (show x)
+  where isPalindromeString :: [Char] -> Bool
+        isPalindromeString []   = True
+        isPalindromeString (a:[]) = True
+        isPalindromeString s    = (head s) == (last s) && (isPalindromeString inner)
+          where inner = tail (init s)
+
+isDivisible :: Integer -> Bool
+isDivisible x = any (\y-> x `mod` y == 0) [999, 998..(ceiling (sqrt (fromIntegral x)))]
 
 f :: Integer -> Integer
-f x = 
+f x 
   | x < 100*100      = -1 --no solution
   | isPalindrome x   = if isDivisible x
                          then x
                          else f x
   | otherwise        = f (x-1)
 
-any (\x -> isPalindrome x && isDivisible x) [999*999..100*100] 
+main = do
+  putStrLn (show (find (\x -> isPalindrome x && isDivisible x) [999*999, (999*999 - 1)..100*100]))
